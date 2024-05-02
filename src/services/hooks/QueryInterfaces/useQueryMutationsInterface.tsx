6e15b7280a50aedd7479e1d3b8c188777910537b
Eduardo Valdes Inerarte <eddremonts86@gmail.com>
2024-05-02 20:17:45 +0200

    feat(services): adding react query mutation

--- src/services/hooks/QueryInterfaces/useQueryMutationsInterface.tsx
+++ src/services/hooks/QueryInterfaces/useQueryMutationsInterface.tsx
@@ -1,5 +1,17 @@
-const useQueryMutationsInterface = async () => {};
+import { useMutation } from "@tanstack/react-query";
+import { AxiosResponse } from "axios";
+
+interface UseQueryInterfaceProps {
+  fn: () => Promise<AxiosResponse<unknown, unknown>>;
+}
+
+const useQueryMutationsInterface = ({ fn }: UseQueryInterfaceProps) => {
+  const response = useMutation({
+    mutationFn: async () => {
+      const { data } = await fn();
+      return data;
+    },
+  });
+  return response;
+};
 export { useQueryMutationsInterface };
-
-
-
